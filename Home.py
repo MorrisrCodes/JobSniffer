7d6e32dc05d9a8b10d751f70f719434ec48fa209
Morris <morrisrcodes@gmail.com>
2023-11-18 11:48:57 -0500

    Combined pages to allow for button usage

--- Home.py
+++ Home.py
@@ -1,9 +1,11 @@
 import streamlit as st
+import pandas as pd
+
 
 # Streamlit layout settings
-st.set_page_config(page_title="Job Sniffer", layout="centered")
+st.set_page_config(page_title="Home", layout="centered")
 
-# Streamlit app layout
+# Streamlit app layout for Home
 st.title(":dog: :green[Job] Sniffer")
 
 # App description
@@ -15,21 +17,61 @@ st.markdown(
     """Sniffing out job opportunities in your emails and delivering them right to your inbox!"""
 )
 
-st.markdown(
-        """
-        ### Unleash the Power of Focus with Job Sniffer
+# Check if the button has been clicked
+if 'button_clicked' not in st.session_state:
+    st.session_state.button_clicked = False
 
-Tailored for job seekers, Job Sniffer sifts through the email clutter, filtering out distractions and pinpointing messages 
-relevant to your employment journey. Whether it's a thrilling acceptance, a challenging technical interview, an online 
-assessment adventure, or an enlightening behavioral evaluation. Job Sniffer's advanced AI features empower you to 
-effortlessly organize and manage your application-related emails. With intelligent categorization and reminders, 
-it ensures that you stay focused on what matters most—your career advancement. Maximize efficiency, minimize stress, 
-and let Job Sniffer be your digital companion on the path to professional success."
+# Display the button only if it hasn't been clicked
+if not st.session_state.button_clicked:
+    clicked = st.button('Login', key='login_button')
+    if clicked:
+        st.session_state.button_clicked = True
 
+# Streamlit app layout for App
+if st.session_state.button_clicked:
+    email_data = [
+        {
+            "Company": "Facebook",
+            "Date": "7/30",
+            "Summary": "No",
+            "Status": "Determination"
+        },
+        {
+            "Company": "Amazon",
+            "Date": "8/23",
+            "Summary": "No",
+            "Status": "Determination"
+        },
+        {
+            "Company": "Apple",
+            "Date": "9/4",
+            "Summary": "Received",
+            "Status": "Awaiting"
+        },
+        {
+            "Company": "Netflix",
+            "Date": "10/14",
+            "Summary": "Yes",
+            "Status": "Interview Scheduled"
+        },
+        {
+            "Company": "Google",
+            "Date": "11/2",
+            "Summary": "Received",
+            "Status": "Awaiting"
+        }
+    ]
 
+    st.title("Job Sniffer - App")
 
+    data = []
 
+    # Iterate through email data and create a DataFrame for each company
+    for i in email_data:
+        df = pd.DataFrame([i], columns=["Company", "Date", "Summary", "Status"])
+        data.append(df)
 
+    # Concatenate the list of DataFrames into a single DataFrame
+    data = pd.concat(data, ignore_index=True)
 
-
-        """)
+    st.dataframe(data, hide_index=True, width=1000)
---
+++ pages/mission.py
@@ -0,0 +1,25 @@
+import streamlit as st
+
+
+st.set_page_config(page_title="Job Sniffer", layout="centered")
+
+st.markdown("<h1 style='text-align: center; color: #59C866;'>Vision</h1>", unsafe_allow_html=True)
+st.subheader("", divider="green",)
+st.markdown('''
+#### Dream with us
+In shaping the future, Job Sniffer envisions a landscape where job seekers effortlessly enhance their application efficiency without compromising submission quality. While the conventional approach often involves pre-filling information, we aim to transcend this model by redefining our perspective on acceptance. Our vision is to empower future job seekers with the ability to invest more time in applications and receive concise, meaningful feedback on their efforts. Job Sniffer seeks to revolutionize the job hunting experience by pushing the boundaries of efficiency, offering a fresh approach that goes beyond existing models, ensuring a more focused and rewarding journey for every user.''')
+
+
+st.markdown("<h1 style='text-align: center; color: white;'>Mission</h1>", unsafe_allow_html=True)
+st.subheader("", divider="green",)
+st.markdown(
+    """
+    #### Unleash the Power of Focus with Job Sniffer
+
+Tailored for job seekers, Job Sniffer sifts through the email clutter, filtering out distractions and pinpointing messages 
+relevant to your employment journey. Whether it's a thrilling acceptance, a challenging technical interview, an online 
+assessment adventure, or an enlightening behavioral evaluation. Job Sniffer's advanced AI features empower you to 
+effortlessly organize and manage your application-related emails. With intelligent categorization and reminders, it ensures that you stay focused on what matters most—your career advancement. Maximize efficiency, minimize stress, 
+and let Job Sniffer be your digital companion on the path to professional success.
+"""
+)
