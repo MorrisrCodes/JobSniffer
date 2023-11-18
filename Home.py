1ce0901ca1da48a8ec25847a8e8a2287df821065
OatmealJester <drrr032@gmail.com>
2023-11-17 23:29:20 -0500

    Created frontend for homepage

---
+++ Home.py
@@ -0,0 +1,26 @@
+import streamlit as st
+
+# Streamlit layout settings
+st.set_page_config(page_title="Job Sniffer", layout="centered")
+
+# Streamlit app layout
+st.title(":dog: :green[Job] Sniffer")
+
+# App description
+st.subheader(
+    "Fetch Your Future with Job Sniffer 🐾",
+    divider="green",
+)
+st.markdown(
+    """Sniffing out job opportunities in your emails and delivering them right to your inbox!"""
+)
+
+st.markdown(
+        """
+        ### Unleash the Power of Focus with Job Sniffer
+
+Tailored for job seekers, Job Sniffer sifts through the email clutter, filtering out distractions and pinpointing messages 
+relevant to your employment journey. Stay on track with our personalized inbox assistant, tracking your application stages 
+seamlessly—whether it's a rejection, acceptance, technical interview, online assessment, or behavioral evaluation. Simplify
+ your job search, and let Job Sniffer guide you through every step with clarity and efficiency."
+        """)
