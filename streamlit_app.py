3a886871a17b61f2a412c3664d457d68bc70c3c0
bladex_3000 <aledie27@gmail.com>
2023-11-18 17:01:39 -0500

    google sign in hangs after runlocalserver

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,14 +21,52 @@ import mock
 
 # Handle OAuth2 callback route
 AUTHORIZATION_CODE = "code"
-REDIRECT_URI = "http://localhost:8501"  # Replace with your redirect URI
+REDIRECT_URI = "urn:ietf:wg:oauth:2.0:oob"# "http://localhost:8501"
+client_id = "323856597657-o1v0baasi5d7tgkprlkjk0ji4g6sccr3.apps.googleusercontent.com"
+SCOPES = ['openid', 'https://www.googleapis.com/auth/userinfo.email', 'https://www.googleapis.com/auth/userinfo.profile']
 
 # load_dotenv()
 # client_id = os.environ["GOOGLE_CLIENT_ID"]
 # client_secret = os.environ["GOOGLE_CLIENT_SECRET"]
 # redirect_uri = os.environ["GOOGLE_REDIRECT_URI"]
 
-SCOPES = ['openid', 'https://www.googleapis.com/auth/userinfo.email', 'https://www.googleapis.com/auth/userinfo.profile']
+# Function to get user information after Google sign-in
+def google_sign_in(loggedIn):
+    if not loggedIn:
+        if st.button("Sign in with Google"):
+            st.write("TEST1")
+            loggedIn=True
+            # Create a flow instance
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'client_secrets.json',
+                scopes=SCOPES
+            )
+            st.write("TEST2")
+            # Run the OAuth 2.0 authorization flow
+            credentials = flow.run_local_server(
+                host='localhost',
+                port=8501,
+                authorization_prompt_message='Please visit this URL: {url}',
+                success_message='The auth flow is complete; you may close this window.',
+                open_browser=True
+            )
+            st.write("TEST3")
+            # Build the Google API service with the obtained credentials
+            service = build('oauth2', 'v2', credentials=credentials)
+            st.write("TEST4")
+            # Retrieve user information
+            user_info = service.userinfo().get().execute()
+            st.write("TEST5")
+            # Print or use the user information as needed
+            email = user_info.get('email', '')
+            name = user_info.get('name', '')
+            st.write("TEST6")
+            # Display user information
+            st.write(f"Email: {email}")
+            st.write(f"Name: {name}")
+            st.write("TEST")
+    else:
+        st.write("logged in")
 
 # Authenticate to Firestore with the JSON account key.
 db = firestore.Client.from_service_account_json("jobsniffer-firestore-key.json")
@@ -70,15 +108,7 @@ def get_user_email(token):
         print("Invalid ID token.")
         return None
 
-# # Create a reference to the Google post.
-# doc_ref = db.collection("users").document("jobs").collection("Becky").document("Software Engineering Intern, PhD, Summer 2024")
-
-# # Then get the data at that reference.
-# doc = doc_ref.get()
-
-# # Let's see what we got!
-# st.write("The id is: ", doc.id)
-# st.write("The contents are: ", doc.to_dict())
+loggedIn=False
 
 # Now let's make a reference to ALL of the posts
 users_ref = db.collection("users").document("jobs").collection("Becky")
@@ -124,31 +154,42 @@ doc_ref.delete()
 
 # create_user(get_user_email());
 
-if st.button("Sign in with Google"):
-    # Redirect the user to the Google Sign-In page
-    auth_url = "https://accounts.google.com/o/oauth2/auth"
-    client_id = "323856597657-o1v0baasi5d7tgkprlkjk0ji4g6sccr3.apps.googleusercontent.com"  # Replace with your actual client ID
-    redirect_uri = "http://localhost:8501"  # Replace with your redirect URI
-    scope = "openid"  # Replace with the desired scopes
-    state = "state123"  # Replace with a unique state value
-    auth_endpoint = f"{auth_url}?response_type=code&client_id={client_id}&redirect_uri={redirect_uri}&scope={scope}&state={state}"
-    st.markdown(f'<a href="{auth_endpoint}">Click here to sign in with Google</a>', unsafe_allow_html=True)
+# email='email@gmail.com'
+# name='name'
+# if st.button("Sign in with Google"):
+#     # Redirect the user to the Google Sign-In page
+#     auth_url = "https://accounts.google.com/o/oauth2/auth"
+#     client_id = "323856597657-o1v0baasi5d7tgkprlkjk0ji4g6sccr3.apps.googleusercontent.com"  # Replace with your actual client ID
+#     redirect_uri = "http://localhost:8501"  # Replace with your redirect URI
+#     scope = "openid"  # Replace with the desired scopes
+#     state = "state123"  # Replace with a unique state value
+#     auth_endpoint = f"{auth_url}?response_type=code&client_id={client_id}&redirect_uri={redirect_uri}&scope={scope}&state={state}"
+#     st.markdown(f'<a href="{auth_endpoint}">Click here to sign in with Google</a>', unsafe_allow_html=True)
 
-    # creds = mock.Mock(spec=credentials.Credentials)
-    flow = InstalledAppFlow.from_client_secrets_file(
-    'client_secrets.json',
-    scopes=['openid', 'https://www.googleapis.com/auth/userinfo.email', 'https://www.googleapis.com/auth/userinfo.profile'])
+#     # creds = mock.Mock(spec=credentials.Credentials)
+#     flow = InstalledAppFlow.from_client_secrets_file(
+#     'client_secrets.json',
+#     scopes=['openid', 'https://www.googleapis.com/auth/userinfo.email', 'https://www.googleapis.com/auth/userinfo.profile'])
 
-    flow.run_local_server(host='localhost',
-      port=8501, 
-      authorization_prompt_message='Please visit this URL: {url}', 
-      success_message='The auth flow is complete; you may close this window.',
-      open_browser=True)
-    credentials = flow.credentials
+#     flow.run_local_server(host='localhost',
+#         port=8501, 
+#         authorization_prompt_message='Please visit this URL: {url}', 
+#         success_message='The auth flow is complete; you may close this window.',
+#         open_browser=True)
+#     credentials = flow.credentials
 
-    service = build('calendar', 'v3', credentials=credentials)
+#     service = build('calendar', 'v3', credentials=credentials)
 
-    user_info_service = build('oauth2', 'v2', credentials=creds)
-    user_info = user_info_service.userinfo().get().execute()
-    st.write(user_info['email'])
-    st.write(user_info['name'])
+#     user_info_service = build('oauth2', 'v2', credentials=creds)
+#     user_info = user_info_service.userinfo().list().execute()
+#     st.write(user_info['email'])
+#     print(user_info['email'])
+#     st.write(user_info['name'])
+#     st.write("test")
+#     email=user_info['email']
+#     name=user_info['name']
+# st.write("test2")
+# st.write(email+' '+name)
+
+if google_sign_in(loggedIn):
+     st.write("AFTER BUTTON")
